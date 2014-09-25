7a44ff5357c3807ea29ca9e083641a23ebbb6c51
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-25 22:40:11 +0100

    Added station tests

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,11 @@
 class Station 
+
 	def initialize
 		@bikes = []
+	end
+
+	def capacity
+		20
 	end
 
 	def bikes_count
@@ -8,14 +13,24 @@ class Station
 	end
 
 	def dock(bike)
+		raise "The station is full" if full?
 		@bikes << bike
 	end
 
 	def release(bike)
+		raise "The station is empty!" if bikes_count == 0
 		@bikes.delete(bike)
 	end
 
 	def available_bikes
 		@bikes.reject{|bike| bike.broken?}
+	end
+
+	def full?
+		bikes_count == capacity
+	end
+
+	def empty?
+		bikes_count == 0
 	end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -4,6 +4,7 @@ describe Station do
 
 	let(:station) {Station.new}
 	let(:bike) {double :bike, broken?: false}
+	let(:bike2) {double :bike2, broken?: false}
 	let(:broken_bike) {double :broken_bike, broken?: true}
 
 	context "Upon initialize, a docking station" do
@@ -20,13 +21,35 @@ describe Station do
 			expect{station.dock(bike)}.to change{station.bikes_count}.by 1
 		end
 
+		it "should know when it's full" do
+			expect(station).not_to be_full
+			fill(station)
+			expect(station).to be_full
+		end
+
+		def fill(station)
+			station.capacity.times{station.dock(bike)}
+		end
+
+		it "should not dock a bike if full" do
+			fill(station)
+			expect{station.dock(bike)}.to raise_error(RuntimeError)
+		end
+
 		it 'can release a bike' do
 			station.dock(bike)
 			expect{station.release(bike)}.to change{station.bikes_count}.by -1
 		end
 
+		it "should know when it's empty" do
+			expect(station).to be_empty
+		end
+
+		it 'can not release a bike if empty' do
+			expect{station.release(bike)}.to raise_error(RuntimeError)
+		end
+
 		it 'should know which bikes are available' do
-			allow(broken_bike).to receive(:break!)
 			[bike, broken_bike].each {|bike| station.dock(bike)}
 			expect(station.available_bikes).to eq([bike])
 		end 
